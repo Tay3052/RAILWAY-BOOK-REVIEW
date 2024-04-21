9263547e56ba7aa3aa6e9432ee1411f0c8b10179
Takumi <fd3s.bcnr33@gmail.com>
2024-04-22 02:58:59 +0900

    4/22

--- src/components/Signout.tsx
+++ src/components/Signout.tsx
@@ -1,15 +1,17 @@
 import Cookies from "js-cookie";
 import { Link } from "react-router-dom";
+import "../assets/scss/TopPage.scss";
 
 const Signout = () => {
   Cookies.remove("token");
   return (
     <>
-      <div>
+      <div className="center">
+        <h1 className="title">ログアウト通知</h1>
         <p>ログアウトしました</p>
       </div>
 
-      <div>
+      <div className="center">
         <Link to="/">トップ画面に戻る</Link>
       </div>
     </>
